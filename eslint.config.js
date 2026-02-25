87d157a025980eb0c2fc725238b1737ac932141a
NastyaSulina <sulina01@mail.ru>
2026-02-25 22:51:40 +0300

    update configs

--- eslint.config.js
+++ eslint.config.js
@@ -9,12 +9,13 @@ import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-    globalIgnores(['dist']),
+    globalIgnores(['dist', 'node_modules']),
     {
-        files: ['**/*.{ts,tsx}'],
+        files: ['src/**/*.{ts,tsx}'],
         extends: [
             js.configs.recommended,
-            tseslint.configs.recommended,
+            // tseslint.configs.recommended,
+            ...tseslint.configs.recommendedTypeChecked,
             reactHooks.configs.flat.recommended,
             reactRefresh.configs.vite,
             reactX.configs['recommended-typescript'],
