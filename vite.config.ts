14ef6a38ada94200ce298a989a51efd3bf4e45ad
NastyaSulina <sulina01@mail.ru>
2026-04-17 17:16:51 +0300

    add setup for express + ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
     plugins: [react(), tsconfigPaths()],
+    server: {
+        proxy: {
+            '/api': 'http://localhost:3001',
+        },
+    },
 })
