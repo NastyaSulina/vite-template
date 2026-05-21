7de3632fbec30c5ea2a32a0873a5f5456627848f
NastyaSulina <sulina01@mail.ru>
2026-05-21 13:07:03 +0300

    add eslint-plugin-import and paths

--- eslint.config.js
+++ eslint.config.js
@@ -6,6 +6,7 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 import reactX from 'eslint-plugin-react-x'
 import reactDom from 'eslint-plugin-react-dom'
 import tseslint from 'typescript-eslint'
+import importPlugin from 'eslint-plugin-import'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -15,6 +16,8 @@ export default defineConfig([
         extends: [
             js.configs.recommended,
             tseslint.configs.recommended,
+            importPlugin.flatConfigs.recommended,
+            importPlugin.flatConfigs.typescript,
             reactHooks.configs.flat.recommended,
             reactRefresh.configs.vite,
             reactX.configs['recommended-typescript'],
@@ -29,9 +32,42 @@ export default defineConfig([
                 tsconfigRootDir: import.meta.dirname,
             },
         },
-
+        settings: {
+            'import/resolver': {
+                typescript: {
+                    project: './tsconfig.app.json',
+                },
+                node: true,
+            },
+        },
         rules: {
             'prettier/prettier': ['error'],
+
+            'import/order': [
+                'error',
+                {
+                    groups: [
+                        'external',
+                        'builtin',
+                        'index',
+                        'sibling',
+                        'parent',
+                        'internal',
+                        'type',
+                    ],
+                    pathGroups: [
+                        { pattern: '@app/**', group: 'internal', position: 'before' },
+                        { pattern: '@pages/**', group: 'internal', position: 'before' },
+                        { pattern: '@widgets/**', group: 'internal', position: 'before' },
+                        { pattern: '@features/**', group: 'internal', position: 'before' },
+                        { pattern: '@entities/**', group: 'internal', position: 'before' },
+                        { pattern: '@shared/**', group: 'internal', position: 'before' },
+                    ],
+                    'newlines-between': 'always',
+                },
+            ],
+            'import/no-extraneous-dependencies': 'off',
+            'import/prefer-default-export': 'off',
         },
     },
 ])
